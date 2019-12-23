533985af3ac4695681c57b5a67a0d94632a3de25
Matas Kondrasovas <M.Kondrasovas@gmail.com>
2019-12-23 21:10:39 +0200

    Add project files.

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApp1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HeyRed.Mime;
+
+namespace CoreApp1.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        //folder where uploads are kept
+        private readonly string StoragePath = "FileStorage";
+
+        public IActionResult Index()
+        {
+
+            return View(Startup.fileUploadModels);
+        }
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // full path to file in temp location
+                    var filePath = Path.Join(StoragePath, formFile.FileName);
+                    //Save File Info
+                    FileUploadModel fileUploadModel = new FileUploadModel()
+                    {
+                        Id = Guid.NewGuid(),
+                        FileName = formFile.FileName,
+                        Created_At = DateTime.Now,
+                        Downloads = 0
+                    };
+                    Startup.fileUploadModels.Add(fileUploadModel);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return RedirectToAction("Index");
+        }
+        //Download Action
+        public async Task<IActionResult> Download(string filename)
+        {
+            if (filename == null)
+                return Content("filename not present");
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), StoragePath, filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+
+            //update download counter
+            Startup.fileUploadModels.FirstOrDefault(q => q.FileName == filename).Downloads++;
+
+            memory.Position = 0;
+            return File(memory, MimeTypesMap.GetMimeType(filename), Path.GetFileName(path));
+        }
+    }
+}
---
+++ Models/FileUploadModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp1.Models
+{
+    public class FileUploadModel
+    {
+        public Guid Id { get; set; }
+        [DisplayName("File")]
+        public string FileName { get; set; }
+        [DisplayName("Upload Date")]
+        public DateTime Created_At { get; set; }
+        public int Downloads { get; set; }
+    }
+}
